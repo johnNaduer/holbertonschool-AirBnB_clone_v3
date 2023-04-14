acb439e7e43c8400e22ebc5f70982dc6473ed44e
johnNaduer <naduer789@gmail.com>
2023-04-14 08:29:54 -0700

    feat: add changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """create route by /status"""
 from api.v1.views import app_views
-
 from flask import jsonify
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
