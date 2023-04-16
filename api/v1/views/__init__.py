c55d90600cb40737feec077cd2b62ca2b050ed46
johnNaduer <naduer789@gmail.com>
2023-04-15 20:21:17 -0700

    feat: add file init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,3 +8,4 @@ wildcard import of everything in the package api.v1.views.index => PEP8 will com
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
+from api.v1.views.states import *
