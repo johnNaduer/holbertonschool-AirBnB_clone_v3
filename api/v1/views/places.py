8004afe59110e1d327c8c719b5dd171917c21ab4
johnNaduer <naduer789@gmail.com>
2023-04-18 16:33:37 -0500

    feat: update coment

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" View for Place objects that handles default API actions """
+"""
+View for Place objects that handles default API actions
+In the file api/v1/views/places.py
+"""
+
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
