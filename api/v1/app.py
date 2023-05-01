c5f9fd2bc534ddeff4229064d79eff3985004b2c
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-05-01 16:29:24 -0700

    fix: include shebang

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -13,4 +15,3 @@ if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
-
